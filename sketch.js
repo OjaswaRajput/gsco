b629b38316c4737cb5fad3302be2756bc47bd358
Ojaswa Rajput <69306443+OjaswaRajput@users.noreply.github.com>
2021-09-27 14:51:19 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,77 +1,55 @@
-var car,carImage;
-var wall,wall2;
-var score;
-var weight;
-var gameState = PLAY;
-var PLAY = 1;
-var END = 0 ;
 
-let  paused = true;
+
+var car,wall;
+
+
+var speed, weight; 
+
 
 function setup() {
-  createCanvas(400,800);
-  car = createSprite(200,100, 15, 30);
-  wall = createSprite(200,650,400,50);
+  createCanvas(1600, 400);
+
+
+	speed=random(55,90)
+	weight=random(400,1500)
+
+
+	car=createSprite(50, 200, 50,50);   
+
+	car.velocityX = speed;
+
+	car.shapeColor=color(255);
+
+
+  
+  	wall=createSprite(1500,200, 60, height/2)
+  	wall.shapeColor=color(80,80,80)
 }
 
-function preload(){
- 
-}
 
 function draw() {
-  background("gray");  
-  wall.shapeColor = "red";
-  car.shapeColor = "white";
-  speed = random(54,90);
-  weight = random(400,1500);
-    car.velocityY = 10;
-     wall2 = createSprite(100,400,800,100);
-     car.depth = wall2.depth
-     car.depth = car.depth
-     car.depth = +1;
-  
-  wall.dbug = true;
-  
-    textSize(25);
-    fill("red");
-    text("weight = "+ weight,2,150);
-    textSize(25);
-    fill("red");
-    text("bullet speed = "+speed,2,200);
-    textSize(25);
-    fill("red");
-    text("Thickness = 50",25,590);
-   score = Math.round(random(10,200,+5));
+  background(0);
 
-  if(car.isTouching){
-   
-      car.velocityY = 0 ;
-    var deformation = 0.5*weight*score*score/22509;
-    if (deformation<100){
-       car.shapeColor =color(255,0,0 );
-  }
-        if (deformation>180){
-       car.shapeColor =color(230,230,0 );
-  }
-        if (deformation<100 && deformation>180){
-       car.shapeColor =color(0,255,0 );
-  }
-    car.y>700;
-    textSize(25);
-    fill("red");
-    text("PRESS F5 OR RESTART PAGE",25,100);
-    textSize(30)
-    fill("blue");
-    text("Damage = " + score,100,300);
-   
-      
-    
-  }
+  if(wall.x-car.x < (car.width+wall.width)/2)
+  {
+  	car.velocityX=0;
+  	var deformation=0.5 * weight * speed* speed/22500;
+	if(deformation>180)
+	{
+		car.shapeColor=color(255,0,0);
+	}
+
+	if(deformation<180 && deformation>100)
+	{
+		car.shapeColor=color(230,230,0);
+	}
+
+	if(deformation<100)
+	{
+		car.shapeColor=color(0,255,0);
+	}
+  }  
   
-  
+  drawSprites();
  
-   
-    drawSprites();  
-  
-
 }
